36d76f14db6714b3d1071312feb3de7775f571ff
abraham-yusuf <1703020028@students.unis.ac.id>
2022-11-15 13:08:03 +0000

    add client

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hello Blockchain Developers!
+        </div>
+
+        <div className="bio">
+        I am Abraham Yusuf and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
